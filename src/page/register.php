a331b863de8635a74304ac6f372ca9fd7abeefac
PhG <info@cogitium.com>
2024-03-27 12:16:56 +0100

    Dev de la page register

---
+++ src/page/register.php
@@ -0,0 +1,16 @@
+<?php
+require 'sql/user.sql.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $hashPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $_POST['password'] = $hashPassword;
+    $isRegistered = registerUser($_POST);
+    header('Location: index.php');
+    exit;
+}
+
+$headerTitle = 'Inscrivez-vous !';
+
+//var_dump($_SERVER); exit;
+
+require 'template/register.tpl.php';
--- src/sql/user.sql.php
+++ src/sql/user.sql.php
@@ -18,3 +18,27 @@ function loginUser($email) {
         die("Erreur SQL : " . $e->getMessage());
     }
 }
+
+function registerUser($user) {
+    global $pdo;
+
+    try {
+        $query = 
+        "INSERT INTO `users`
+            (`lastName`, `firstName`, `email`, `phone`, `password`, `role`)
+        VALUES
+            (:lastName, :firstName, :email, :phone, :password, 'ROLE_USER')";
+
+        $cursor = $pdo->prepare($query);
+        $cursor->bindParam(':lastName', $user['lastName'], PDO::PARAM_STR);
+        $cursor->bindParam(':firstName', $user['firstName'], PDO::PARAM_STR);
+        $cursor->bindParam(':email', $user['email'], PDO::PARAM_STR);
+        $cursor->bindParam(':phone', $user['phone'], PDO::PARAM_STR);
+        $cursor->bindParam(':password', $user['password'], PDO::PARAM_STR);
+        $cursor->execute();
+        
+        return TRUE;
+    } catch (PDOException $e) {
+        die("Erreur SQL : " . $e->getMessage());
+    }
+}
