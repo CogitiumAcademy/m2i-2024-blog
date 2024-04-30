707f265f46d5a88da44fabb8a5e1e2b09cbc9758
PhG <info@cogitium.com>
2024-04-30 15:10:30 +0200

    Réglage des attributs de la connexion PDO

--- src/sql/pdoConnect.inc.php
+++ src/sql/pdoConnect.inc.php
@@ -4,6 +4,10 @@ function pdoConnect() {
     global $options;
     try {
         $dbh = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD, $options);
+
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
         return $dbh;
         //var_dump($dbh);
     } catch (PDOException $e) {
